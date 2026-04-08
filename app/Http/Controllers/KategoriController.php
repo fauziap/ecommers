9da8778af4c051b0cb7d15a5e28de746b0aa48cc
fauziap <af644537@gmail.com>
2026-04-09 01:43:33 +0700

    first commit

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategori;
+use App\Models\Produk;
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $kategori = Kategori::orderBy('nama_kategori', 'asc')->get();
+        return view('backend.v_kategori.index', [
+            'judul' => 'Kategori',
+            'index' => $kategori
+        ]);
+    }
+
+    public function produkKategori($id)
+    {
+        $kategori = Kategori::orderBy('nama_kategori', 'desc')->get();
+        $produk = Produk::where('kategori_id', $id)->where('status', 1)->orderBy('updated_at', 'desc')->paginate(6);
+        return view('v_produk.produkkategori', [
+            'judul' => 'Filter Kategori',
+            'kategori' => $kategori,
+            'produk' => $produk,
+        ]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('backend.v_kategori.create', [
+            'judul' => 'Kategori',
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request);
+        $validatedData = $request->validate([
+            'nama_kategori' => 'required|max:255|unique:kategori',
+        ]);
+        Kategori::create($validatedData);
+        return redirect()->route('backend.kategori.index')->with('success', 'Data berhasil tersimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $kategori = Kategori::find($id);
+        return view('backend.v_kategori.edit', [
+            'judul' => 'Kategori',
+            'edit' => $kategori
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $kategori = Kategori::findOrFail($id);
+        $rules = [
+            'nama_kategori' => 'required|max:255|unique:kategori',
+        ];
+        $validatedData = $request->validate($rules);
+        $kategori->update($validatedData);
+        return redirect()->route('backend.kategori.index')->with('success', 'Data berhasil diperbaharui');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = kategori::findOrFail($id);
+        $user->delete();
+        return redirect()->route('backend.kategori.index')->with('success', 'Data berhasil dihapus');
+    }
+}
